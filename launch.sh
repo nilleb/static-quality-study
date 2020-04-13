3089464d955899eea9d4b893ba3f1b77d1b00245
Ivo Bellin Salarin <ivo@lumapps.com>
2020-04-13 14:21:32 +0200

    chore: try to execute this

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 # before executing this script, make sure you have edited analysis-parameters.xml
 python3 -m venv venv
 . venv/bin/activate
+pip install -r requirements.txt
 python3 1-extract-issues-from-jira.py
 python3 2-extract-issues-from-sources.py
 python3 3-aggregate-coverage-and-issues.py
